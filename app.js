15b4356f542508bb7d941d39d456ced3f5f5fb27
Just-Nico-pk <miguelnicolas09@aragon.unam.mx>
2024-08-26 01:31:18 -0600

    Creacion de la pagina web con html y java para el prompt

---
+++ app.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Escribe tu nombre");
+let edad = prompt("Escribe tu edad");
+let fechaN = prompt("Escribe tu fecha de nacimiento dd/mm/aaaa");
+console.log("Tu nombre es: " + nombre);
+console.log("Tu edad es: " + edad);
+console.log("Tu fecha de nacimiento es: " + fechaN);
+
